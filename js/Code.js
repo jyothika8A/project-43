34ae7e8024b32fbe3c0748bc1ca77d0f2567eadb
jyothika8A <89692218+jyothika8A@users.noreply.github.com>
2022-06-04 20:39:39 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("A E V R I L B A", 100,50)
+    fill("lightblue")
+    text("Hint: A value that can change !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("O T U N C I N F", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a well defined task !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("S A E D T A B A", 100,250)
+    fill("lightblue")
+    text("Hint: Collection of information !!", 100,270)
+
+}
